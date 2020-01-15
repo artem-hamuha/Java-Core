13802e4e87ade14cedd5d4ba52204142cf744d6e
Artem Hamuha <artem.hamuha@gmail.com>
2020-01-14 16:48:14 -0800

    i played around with min and max in Math and also used float and int variables with Math

---
+++ src/Java30.java
@@ -0,0 +1,10 @@
+public class Java30 {
+    public static void main(String[] args) {
+        float num1 = 24.75f ;
+        int num2 = 25 ;
+
+        System.out.println("Most is " + Math.max(num1 , num2));
+
+        System.out.println("Least is " + Math.min(num1 , num2));
+    }
+}
