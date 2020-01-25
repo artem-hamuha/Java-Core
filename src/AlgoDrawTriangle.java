f9710ef6981b536d765051069162408f696326aa
Artem Hamuha <artem.hamuha@gmail.com>
2020-01-24 22:41:47 -0800

    random loops(literally practice)

---
+++ src/AlgoDrawTriangle.java
@@ -0,0 +1,25 @@
+public class AlgoDrawTriangle {
+
+    //      task:
+    //      ================================
+    //      1*****
+    //      12****
+    //      123***
+    //      1234**
+    //      12345*
+
+
+
+
+    public static void main(String[] args) {
+        for (int i=1;i<=6; i++) {
+            for (int j=1;j<=i; j++) {
+                System.out.print(j);
+            }
+            for (int k=6; k>i; k--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Java39.java
@@ -0,0 +1,15 @@
+/*public class Java39 {
+    public static void main(String[] args) {
+        String txt = "This is a local variable in the main method";     //create and output var
+        System.out.println(txt);
+
+        public static void sub(){
+            String txt = "This is a variable in the sub method";       //do the same w/variable as in main method
+        }
+        sub();  //call sub method
+
+        final static String txt = "This ia global variable of the scope class";
+
+        System.out.println(Scope.txt);
+    }
+}*/
