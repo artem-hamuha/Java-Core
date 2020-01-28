bf88a41d6e4cc194c92f5cc861f42463df3d2ffc
Artem Hamuha <artem.hamuha@gmail.com>
2020-01-27 19:28:03 -0800

    producing an object instance

---
+++ src/Java44.java
@@ -0,0 +1,8 @@
+public class Java44 {
+    public final static String color = "Red";
+    public final static String bodyType = "Coupe";
+    public static String accelerate(){
+        String motion = "Accelerating...";
+        return motion;
+    }
+}
---
+++ src/JavaSub44.java
@@ -0,0 +1,13 @@
+public class JavaSub44 {
+    public static void main(String[] args) {
+        System.out.println("Paint is " + Java44.color);
+        System.out.println("Style is " + Java44.bodyType);
+        System.out.println(Java44.accelerate());
+
+        Java44 Porsche = new Java44();
+
+        System.out.println("Paint is " + Porsche.color);
+        System.out.println("Style is " + Porsche.bodyType);
+        System.out.println(Porsche.accelerate());
+    }
+}
