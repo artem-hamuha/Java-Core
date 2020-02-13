975e988556cc07b414ddd9f3d0d0d40d32ae86aa
Artem Hamuha <artem.hamuha@gmail.com>
2020-02-12 18:56:26 -0800

    just watchinf a tutorial to get info part 1

---
+++ src/Video1.java
@@ -0,0 +1,10 @@
+public class Video1 {
+    public static void main(String[] args) {
+        System.out.println("Sup , just watching a full out " +
+                "video of a Java tutorial");
+        int age = 12 ;  // you could declare a couple vars on 1 line
+        System.out.println(age); //But its not recommended
+        age = 13 ;  //u could also have 1 var equal to another
+        System.out.println(age);
+    }
+}
