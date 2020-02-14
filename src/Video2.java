224a011ee7c1baa1b62c9730f50f05762f29ab99
Artem Hamuha <artem.hamuha@gmail.com>
2020-02-13 21:46:27 -0800

    just watching a tutorial to get info part 2

---
+++ src/Video2.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Video2 {
+    public static void main(String[] args) {
+        long b = 3_467_868_777L;    //the L represents long
+        float d = 10.99F;   //the F represents float
+    }
+    public void hi(){
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
