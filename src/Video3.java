c119a294aaa411f0e9b3d7f1d6da3b7ac269897e
Artem Hamuha <artem.hamuha@gmail.com>
2020-02-16 17:53:43 -0800

    just watching a tutorial to get info part 3 and 1/2

--- src/Video3.java
+++ src/Video3.java
@@ -1,8 +1,16 @@
+import java.awt.*;
+
 public class Video3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) {    //this doesn't show the change
         int x = 1;
         int y = x;  //y equals 1
         x = 2;
         System.out.println(y);  //y still = 1 (very cool)
+    }
+    public void dvd(){  //this shows the change
+        Point point1 = new Point(1,1) ;
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
     }
 }
