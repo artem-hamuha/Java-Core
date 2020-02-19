0eaa7a5def402833ffd5adc985ad27d8f128bfe4
Artem Hamuha <artem.hamuha@gmail.com>
2020-02-18 18:44:51 -0800

    just watching a tutorial to get info part 5

---
+++ src/Video5.java
@@ -0,0 +1,6 @@
+public class Video5 {
+    public static void main(String[] args) {
+        String message = "\\Hi \"you\"";     //backslashes help a lot and 2 backslashes get you one when you output your string
+        System.out.println(message);    //also there are a lot more like \t \n and the list goes on
+    }
+}
